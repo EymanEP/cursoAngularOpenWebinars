51b45c23777e319cfa079938f81777a29ef4383d
EymanEP <ayenaxx@gmail.com>
2022-12-04 21:20:38 +0100

    angular entradas de .json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { ListadoComponent } from './views/listado/listado.component';
 import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
 import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+import { LoginComponent } from './views/login/login.component';
+
 
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+
   { path: 'listado', component: ListadoComponent },
   { path: 'nosotros', component: AcercaDeNosotrosComponent },
 
